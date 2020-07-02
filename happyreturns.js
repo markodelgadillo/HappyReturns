b6baae401c6cfd2158301f16b9b365900bbd8262
marko delgadillo <markoneseven@gmail.com>
2020-07-02 14:30:43 -0700

    first commit

---
+++ happyreturns.js
@@ -0,0 +1,7 @@
+// perform a GET to the endpoint
+// https://happyreturnsqatest.free.beeceptor.com/getProductVariants
+
+// Map a variant object from the variants array from the GET to a JSON object
+
+// Perform a POST using the object as the request body to the endpoing:
+// https://happyreturnsqatest.free.beeceptor.com/order
